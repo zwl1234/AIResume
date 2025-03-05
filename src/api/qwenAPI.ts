c5f25462c8efcb33b3955a72433d72e6fac23928
zhao <2911396166@qq.com>
2025-03-05 18:31:53 +0800

    fix:配置了setting信息后，无法实时变更，而是需要刷新页面才能变更信息 fixes #3

--- src/api/qwenAPI.ts
+++ src/api/qwenAPI.ts
@@ -1,14 +1,16 @@
+import { computed } from "vue";
 import { useSettingsStore } from "../store/useSettingsStore";
 import type { DialogueHistory } from "../types/aiDialogue";
 import { WorkerPool } from "../worker/workerPool";
+
 //读取用户设置的API地址和API Key
 const settingsStore = useSettingsStore();
-const API_URL = settingsStore.aliApiUrl;
-const userApiKey = settingsStore.aliApiKey;
-const model = settingsStore.modelName;
+const API_URL = computed(() => settingsStore.aliApiUrl);
+const userApiKey = computed(() => settingsStore.aliApiKey);
+const model = computed(() => settingsStore.modelName);
 // 创建线程池，最多 4 个工作线程
 const workerPool = new WorkerPool(4);
 export async function sendToQwenAIDialogue(messages: DialogueHistory,
   onResponse: (responseText: string, isComplete: boolean) => void): Promise<void> {
-  workerPool.execute(messages, userApiKey, model, API_URL, onResponse);
+  workerPool.execute(messages, userApiKey.value, model.value, API_URL.value, onResponse);
 }
